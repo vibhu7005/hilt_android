1ed6f2855d02341d20e7e624b66a9cf303ca227b
vaibhav <vaibhavg7005@gmail.com>
2024-02-12 03:51:58 +0530

    android di hilt setup

---
+++ app/src/main/java/com/example/android_dependency_injection/jordieeApplication.kt
@@ -0,0 +1,8 @@
+package com.example.android_dependency_injection
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+//mandatory for hilt
+@HiltAndroidApp
+class JordieeApplication : Application()
