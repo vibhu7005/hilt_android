7b58fc6d2b452a735be178132d73c563d2f3dba3
vaibhav <vaibhavg7005@gmail.com>
2024-02-27 11:02:11 +0530

    android di hilt modules, binds, provides usecases

--- app/src/main/java/com/example/android_dependency_injection/hilt/NetworkModule.kt
+++ app/src/main/java/com/example/android_dependency_injection/hilt/NetworkModule.kt
@@ -8,6 +8,7 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ActivityComponent
 import dagger.hilt.android.components.ViewComponent
+import dagger.hilt.android.scopes.ActivityScoped
 
 @Module
 @InstallIn(ActivityComponent ::class, ViewComponent::class)
@@ -26,6 +27,7 @@ class NetworkModule {
     }
 
     @Provides
+    @ActivityScoped
     @JordieeResponseIntereptor
     fun provideResponseNetworkService() : NetworkService {
         return NetworkService.Builder().host("jordiee")
--- app/src/main/java/com/example/android_dependency_injection/jordieeApplication.kt
+++ app/src/main/java/com/example/android_dependency_injection/jordieeApplication.kt
@@ -1,8 +1,16 @@
 package com.example.android_dependency_injection
 
 import android.app.Application
+import com.example.android_dependency_injection.hilt.JordieeCallInterceptor
+import com.example.android_dependency_injection.network.NetworkService
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 //mandatory for hilt
 @HiltAndroidApp
-class JordieeApplication : Application()
+class JordieeApplication : Application() {
+
+    @Inject
+    @JordieeCallInterceptor
+    lateinit var networkService: NetworkService
+}
